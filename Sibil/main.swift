06eb09ffc6dbf596d46c8723659596cd9fd0f024
Eryn Wells <eryn@erynwells.me>
2016-12-16 08:52:16 -0800

    Add an Xcode project

---
+++ Sibil/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sibil
+//
+//  Created by Eryn Wells on 12/16/16.
+//  Copyright © 2016 Eryn Wells. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
