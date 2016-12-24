e6ca40122e4584c53a7aaaf93338c3001df0a3c6
Eryn Wells <eryn@erynwells.me>
2016-12-23 17:46:36 -0700

    Print out tokens as they come

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,7 @@ fn main() {
     lexer::hello(&s);
 
     let lexer = lexer::Lexer::new(String::from("((abc))"));
-    for t in lexer { }
+    for t in lexer {
+        println!("token = {}", t);
+    }
 }
