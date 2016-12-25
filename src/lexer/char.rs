c925939faf8949515d7157cc05386b5d4514e23a
Eryn Wells <eryn@erynwells.me>
2016-12-25 12:24:31 -0700

    Break apart is_boolean test into true/false variants

--- src/lexer/char.rs
+++ src/lexer/char.rs
@@ -11,7 +11,8 @@ pub trait Lexable {
     fn is_identifier_subsequent(&self) -> bool;
     fn is_identifier_single(&self) -> bool;
     fn is_hash(&self) -> bool;
-    fn is_boolean(&self) -> bool;
+    fn is_boolean_true(&self) -> bool;
+    fn is_boolean_false(&self) -> bool;
     fn is_newline(&self) -> bool;
 }
 
@@ -40,8 +41,12 @@ impl Lexable for char {
         *self == '#'
     }
 
-    fn is_boolean(&self) -> bool {
-        *self == 't' || *self == 'f'
+    fn is_boolean_true(&self) -> bool {
+        *self == 't'
+    }
+
+    fn is_boolean_false(&self) -> bool {
+        *self == 'f'
     }
 
     fn is_newline(&self) -> bool {
