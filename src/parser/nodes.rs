4cfd0510e66b74c3b52549a7e6f1c47b6776bcf5
Eryn Wells <eryn@erynwells.me>
2016-12-29 12:33:20 -0500

    Define a Constant node for constants

--- src/parser/nodes.rs
+++ src/parser/nodes.rs
@@ -2,4 +2,18 @@
  * Eryn Wells <eryn@erynwells.me>
  */
 
-use types::boolean::Boolean;
+use types::{Boolean, Number};
+
+trait ConstantValue {}
+impl ConstantValue for Boolean {}
+impl ConstantValue for Number {}
+
+struct Constant<V: ConstantValue> {
+    value: V
+}
+
+impl<V: ConstantValue> Constant<V> {
+    fn new(value: V) -> Constant<V> {
+        Constant { value: value }
+    }
+}
