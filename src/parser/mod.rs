2f8f71fc28872201f0a7c038de7c1e7b1216b84e
Eryn Wells <eryn@erynwells.me>
2017-01-02 12:17:26 -0500

    A Program node, and an empty parse() method on Parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,12 +2,34 @@
  * Eryn Wells <eryn@erynwells.me>
  */
 
+mod nodes;
+
+use lexer::Lexer;
+use self::nodes::Program;
+
 pub struct Parser {
+    lexer: Lexer,
 }
 
 impl Parser {
+    pub fn new(lexer: Lexer) -> Parser {
+        Parser { lexer: lexer }
+    }
+
+    pub fn parse(&mut self) -> Program {
+        Program { }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use super::nodes::*;
+    use lexer::Lexer;
+
+    #[test]
+    fn parses_empty_input() {
+        let mut parser = Parser::new(Lexer::new(""));
+        assert_eq!(parser.parse(), Program::new());
+    }
 }
---
+++ src/parser/nodes.rs
@@ -0,0 +1,12 @@
+/* parser/nodes.rs
+ * Eryn Wells <eryn@erynwells.me>
+ */
+
+#[derive(Debug, PartialEq)]
+pub struct Program { }
+
+impl Program {
+    pub fn new() -> Program {
+        Program { }
+    }
+}
