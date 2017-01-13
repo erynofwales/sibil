5ec2a11df19dd03ea87c0c10426a85cf2409e730
Eryn Wells <eryn@erynwells.me>
2017-01-12 21:17:48 -0800

    Add Expression type, Program is a list of Expressions

--- src/parser/nodes.rs
+++ src/parser/nodes.rs
@@ -2,11 +2,21 @@
  * Eryn Wells <eryn@erynwells.me>
  */
 
-#[derive(Debug, PartialEq)]
-pub struct Program { }
+use lexer;
+use types;
+
+pub struct Program {
+    forms: Vec<Expression>
+}
 
 impl Program {
-    pub fn new() -> Program {
-        Program { }
+    pub fn new(forms: Vec<Expression>) -> Program {
+        Program { forms: forms }
     }
+}
+
+pub enum Expression {
+    EOF,
+    Atom(Box<types::Value>),
+    List { left: lexer::Token, expression: Vec<Box<Expression>>, right: lexer::Token },
 }
