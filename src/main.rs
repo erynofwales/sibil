8bd71a144d05bb2a232052041c112f568cb0d3c1
Eryn Wells <eryn@erynwells.me>
2017-01-12 21:18:32 -0800

    Parse the lexed input into a Program in main

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ mod parser;
 mod types;
 
 use lexer::lex;
+use parser::Parser;
 
 fn main() {
-    let lexer = lex("((abc def + ghi #f))");
-    for t in lexer {
-        println!("token = {:?}", t);
-    }
+    let lexer = lex("(#f (abc def + ghi #f))");
+    let mut parser = Parser::new(lexer);
+    let program = parser.parse();
 }
