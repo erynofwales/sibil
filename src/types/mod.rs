90088ae8143844dcdcebc58c3744e72b57970a97
Eryn Wells <eryn@erynwells.me>
2017-04-03 16:20:06 -0400

    Fix up types tests

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -18,19 +18,19 @@ mod tests {
 
     #[test]
     fn booleans_are_equal() {
-        assert_eq!(Bool(true), Bool(true));
-        assert_eq!(Bool(false), Bool(false));
-        assert_ne!(Bool(true), Bool(false));
+        assert_eq!(Bool::new(true), Bool::new(true));
+        assert_eq!(Bool::new(false), Bool::new(false));
+        assert_ne!(Bool::new(true), Bool::new(false));
     }
 
     #[test]
     fn equal_chars_are_equal() {
-        assert_eq!(Char('a'), Char('a'));
-        assert_eq!(Char('a').as_value(), Char('a').as_value());
+        assert_eq!(Char::new('a'), Char::new('a'));
+        assert_eq!(Char::new('a').as_value(), Char::new('a').as_value());
     }
 
     #[test]
     fn booleans_and_chars_are_not_equal() {
-        assert_ne!(Bool(true).as_value(), Char('a').as_value());
+        assert_ne!(Bool::new(true).as_value(), Char::new('a').as_value());
     }
 }
