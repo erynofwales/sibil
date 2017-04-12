1b816e19df7fd2a3c0c26bcbec691ba06667f194
Eryn Wells <eryn@erynwells.me>
2017-04-12 09:15:28 -0700

    Add a failing test to add rationals

--- types/src/number/add.rs
+++ types/src/number/add.rs
@@ -1,4 +1,6 @@
-
+/* types/src/number/add.rs
+ * Eryn Wells <eryn@erynwells.me>
+ */
 
 use std::ops::Add;
 use super::{Int, Flt, Real};
@@ -9,6 +11,7 @@ impl Add for Real {
     fn add(self, other: Real) -> Real {
         match (self, other) {
             (Real::Integer(v), Real::Integer(ov)) => Real::Integer(v + ov),
+            // TODO: The rest.
             _ => Real::Integer(0)
         }
     }
@@ -20,8 +23,14 @@ mod tests {
     use number::Real;
 
     #[test]
-    fn integer_add_works() {
+    fn integer_addition_works() {
         let result = Real::Integer(3) + Real::Integer(5);
         assert_eq!(result, Real::Integer(8));
+    }
+
+    #[test]
+    fn rational_addition_works() {
+        let result = Real::Rational(1, 4) + Real::Rational(1, 4);
+        assert_eq!(result, Real::Rational(1, 2));
     }
 }
