234b60dde9e14fffd74a6b09d27eb177be1c81c5
Eryn Wells <eryn@erynwells.me>
2018-08-20 16:31:10 -0700

    Some spacing tweaks to predicates

--- types/src/predicates.rs
+++ types/src/predicates.rs
@@ -20,12 +20,16 @@ pub trait IsChar {
 pub trait IsNumber {
     /// Is this thing a number?
     fn is_number(&self) -> bool { self.is_complex() || self.is_real() || self.is_rational() || self.is_integer() }
+
     /// Should return `true` if this Value is a complex number type.
     fn is_complex(&self) -> bool { self.is_real() }
+
     /// Should return `true` if this Value is a real number type.
     fn is_real(&self) -> bool { self.is_rational() }
+
     /// Should return `true` if this Value is a rational number type.
     fn is_rational(&self) -> bool { self.is_integer() }
+
     /// Should return `true` if this Value is a integer number type.
     fn is_integer(&self) -> bool { false }
 }
