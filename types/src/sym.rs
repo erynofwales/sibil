d825d0ec8a954692b9df9ab4cb5f2656238dac24
Eryn Wells <eryn@erynwells.me>
2018-08-23 17:05:29 -0700

    [types] Clean up Pair and Sym types
    Reconfigure the top-level object types.
    - Obj is an enum pointer type
    - Object is a trait that all Scheme types should implement
    
    Define Sym, a symbol type.
    Define Pair, a pair/cons/list type.
    
    Implement Display for all types above. Implement casting methods for the above.

---
+++ types/src/sym.rs
@@ -0,0 +1,22 @@
+/* types/src/symbol.rs
+ * Eryn Wells <eryn@erynwells.me>
+ */
+
+use std::any::Any;
+use std::fmt;
+use object::Object;
+use super::*;
+
+pub struct Sym(String);
+
+impl Object for Sym {
+    fn as_any(&self) -> &Any { self }
+    fn as_pair(&self) -> Option<&Pair> { None }
+    fn as_sym(&self) -> Option<&Sym> { Some(self) }
+}
+
+impl fmt::Display for Sym {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
