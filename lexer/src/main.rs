1304e048089f2937dd1209f6f80c21006686b943
Eryn Wells <eryn@erynwells.me>
2018-08-26 13:45:52 -0700

    [lexer] Bring over the REPL from the parser

--- lexer/src/main.rs
+++ lexer/src/main.rs
@@ -4,11 +4,27 @@
 
 extern crate sibillexer;
 
+use std::io::prelude::*;
+use std::io;
 use sibillexer::Lexer;
 
 fn main() {
-    let lexer = Lexer::new("(ab (cd) ef)".chars());
-    for tok in lexer {
-        println!("found {:?}", tok.unwrap());
+    loop {
+        // Print a prompt.
+        print!("> ");
+        io::stdout().flush().ok().expect("couldn't flush");
+
+        // Read a line from stdin.
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        // Create a lexer and parser and process the input.
+        let lexer = Lexer::new(input.chars());
+
+        // Print the parser's output.
+        for thing in lexer {
+            println!("{:?}", thing);
+        }
+        println!();
     }
 }
