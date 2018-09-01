3bdf14720ceffddc6940872be43e262d9176712f
Eryn Wells <eryn@erynwells.me>
2018-09-01 12:03:26 -0700

    [types] Expand the documentation of Numbers

--- types/src/number/mod.rs
+++ types/src/number/mod.rs
@@ -2,9 +2,14 @@
  * Eryn Wells <eryn@erynwells.me>
  */
 
-/// # Numbers
-///
-/// Scheme numbers are complex, literally.
+//! # Numbers
+//!
+//! Scheme numbers are complex, literally. The model it uses is a hierarchy of types called the
+//! Number Tower. It consists of four types, in order: Integers, Rationals (or Fractionals),
+//! Irrationals (or Reals), and Complex Numbers. Each type going down the tower can be
+//! unequivocally cast to the type below it, but the reverse is not necessarily true. So, an
+//! Integer can be cast as a Rational (by putting its value over 1), but a Rational like 1/2 cannot
+//! be represented as an Integer.
 
 mod integer;
 
